67fc85d61eaa75ef18553b160de96ce93a777fed
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-07-29 16:57:38 +0900

    feat: 상수 네이밍 변경 상수 사용값 정리

--- src/constants/scene.ts
+++ src/constants/scene.ts
@@ -1,15 +1,14 @@
 // SCENE
 export const DEFAULT_SCENE_BACKGROUND = 0x353535;
 
+// GRID
+export const DEFAULT_GRID_SIZE = 1000;
+export const DEFAULT_GRID_DIVISION = 2000;
+
 // FOG
 export const DEFAULT_FOG_COLOR = 0x353535;
 export const DEFAULT_FOG_NEAR = 0.1;
 export const DEFAULT_FOG_FAR = 75;
-
-// GRID
-export const GRID_SIZE = 1000;
-export const GRID_DIVISION = 2000;
-export const GRID_COLOR = 0x757575;
 
 // LIGHT
 export const DEFAULT_LIGHT_COLOR = 0xffffff;
@@ -21,9 +20,9 @@ export const DEFAULT_LIGHT_POSITION = {
 };
 
 // CAMERA
-export const CAMERA_FOV = 75;
-export const CAMERA_NEAR = 0.1;
-export const CAMERA_FAR = 1000;
+export const DEFAULT_CAMERA_FOV = 75;
+export const DEFAULT_CAMERA_NEAR = 0.1;
+export const DEFAULT_CAMERA_FAR = 1000;
 export const DEFAULT_CAMERA_POSITION = {
   x: 0,
   y: 0,
